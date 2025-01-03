09f83ee91dae179a60837a9b787e951e526d3d8e
Lightdemon 17 <lightdemon17@gmail.com>
2025-01-03 20:29:53 +0700

    save

--- src/service/date/diff.rs
+++ src/service/date/diff.rs
@@ -4,8 +4,10 @@ use chrono::prelude::*;
 pub fn diff_month(date1: String, date2: String) -> Result<i32, anyhow::Error> {
     let date1 = NaiveDate::parse_from_str(&date1, "%Y-%m-%d")?;
     let date2 = NaiveDate::parse_from_str(&date2, "%Y-%m-%d")?;
+    let year1: i32 = date1.year();
+    let year2: i32 = date2.year();
     let month1: i32 = date1.month() as i32;
     let month2: i32 = date2.month() as i32;
 
-    Ok((month2 - month1).abs() +1)
+    Ok((year2 - year1) * 12 + (month2 - month1) +1)
 }
